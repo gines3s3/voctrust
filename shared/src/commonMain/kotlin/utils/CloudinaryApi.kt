3ec8a59e7e4b8cd25dfd6bc078e1e4e1379ad074
Ganesh T <ganesh.thillaigovind@gendigital.com>
2025-04-06 16:47:06 +0530

    API calls with Ktor

--- shared/src/commonMain/kotlin/utils/CloudinaryApi.kt
+++ shared/src/commonMain/kotlin/utils/CloudinaryApi.kt
@@ -2,30 +2,37 @@
 
     import io.ktor.client.HttpClient
     import io.ktor.client.call.body
+    import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
     import io.ktor.client.request.get
+    import io.ktor.client.request.parameter
+    import io.ktor.client.request.url
+    import io.ktor.serialization.kotlinx.json.json
     import kotlinx.serialization.json.Json
     import model.ResourceResponse
 
     object CloudinaryApi {
 
 
-        private val httpClient = HttpClient()
+        private val httpClient = HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    coerceInputValues = true
+                })
+            }
+        }
 
         suspend fun fetchMedia(mediaType: MediaType, nextCursor: String? = null): ResourceResponse {
-            var resourceResponse = ResourceResponse(emptyList(), null)
             return try {
-                val baseUrl = getBaseUrl(mediaType.workerName)
-                val url = if (nextCursor != null) "$baseUrl?next_cursor=$nextCursor" else baseUrl
-                val responseText = httpClient.get(url).body<String>()
-                try {
-                    resourceResponse = Json.decodeFromString<ResourceResponse>(responseText)
-                } catch (e: Exception) {
-                    println("1 " + e.message)
-                }
-                resourceResponse
+                httpClient.get {
+                    url(getBaseUrl(mediaType.workerName))
+                    nextCursor?.let { cursor ->
+                        parameter("next_cursor", cursor)
+                    }
+                }.body()
             } catch (e: Exception) {
-                println("2 " + e.message)
-                resourceResponse
+                println("Error fetching media: ${e.message}")
+                ResourceResponse(emptyList(), null)
             }
         }
 
