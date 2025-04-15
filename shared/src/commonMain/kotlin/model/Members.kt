8612f9c5f28d1045a2a029ee437ac7cdb0137def
Ganesh T <ganesh.thillaigovind@gendigital.com>
2025-04-15 13:08:02 +0530

    Add email contact link for trustee member

--- shared/src/commonMain/kotlin/model/Members.kt
+++ shared/src/commonMain/kotlin/model/Members.kt
@@ -7,11 +7,15 @@ object Members {
         title = "TRUSTEE",
         location = "Cuddalore, Tamil Nadu",
         links = listOf(
-
             Member.Link(
                 href = "tel:+9194873806440",
                 label = "Call Us",
                 linkType = LinkType.Phone
+            ),
+            Member.Link(
+                href = "mailto:vocyouthsocialwelfaretrust@gmail.com",
+                label = "Write Us",
+                linkType = LinkType.Email
             ),
             Member.Link(
                 href = "https://www.instagram.com/vocyouthsocialwelfare/",
