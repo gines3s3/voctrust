e65cf1f2091b446ae68c2eaa09964a80c935c417
Ganesh T <gines.thilla@gmail.com>
2025-05-03 09:50:21 +0530

    Refactored Ktor and Coil dependencies to libs.versions.toml as bundles

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -35,15 +35,10 @@ kotlin {
                 implementation(compose.components.resources)
 
                 // Ktor
-                implementation("io.ktor:ktor-client-core:3.1.0")
-                implementation("io.ktor:ktor-client-cio:3.1.0")
-                implementation("io.ktor:ktor-client-content-negotiation:3.1.0")
-                implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.0")
+                implementation(libs.bundles.ktor)
 
                 // Coil (Image Loading)
-                implementation("io.coil-kt.coil3:coil-compose:3.1.0")
-                implementation("io.coil-kt.coil3:coil-network-ktor3:3.1.0")
-
+                implementation(libs.bundles.coil)
             }
         }
 
