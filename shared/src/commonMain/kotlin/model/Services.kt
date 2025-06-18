4396899852e9866f37988fc66113ebb56d5e4216
Ganesh T <ganesh.thillaigovind@gendigital.com>
2025-06-18 14:49:51 +0530

    Removed unnecessary code

--- shared/src/commonMain/kotlin/model/Services.kt
+++ shared/src/commonMain/kotlin/model/Services.kt
@@ -6,11 +6,6 @@ object Services {
         description = "We rescue and care for injured, abandoned, and stray animals—especially dogs. Our mission is to ensure every animal gets the love, shelter, and treatment it deserves."
     )
 
-    val templeRestoration = Service(
-        title = "Temple Restoration",
-        description = "Preserving spiritual heritage through the restoration of ancient and neglected temples. We revive cultural landmarks to keep traditions alive for future generations."
-    )
-
     val humanitarianAid = Service(
         title = "Humanitarian Aid & Philanthropy",
         description = "Supporting underprivileged children and the elderly with food, clothing, education, and care. We believe in restoring dignity and providing hope through selfless giving."
